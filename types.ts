5ce2d211b28464171184a6ee81d9959c9dd3afe3
ksielyov <mihailkiseliow@yandex.ru>
2020-12-06 21:18:57 +0500

    Init project

---
+++ types.ts
@@ -0,0 +1,14 @@
+const isFetching: boolean = false;
+const isLoading: boolean = true
+
+let int: number = 19;
+
+const anyFloat: number = 9.5;
+const num: number = 2e5;
+const anyText: string = 'fsdsfd sfsd f sfs ';
+const anyOne: null = null;
+
+const arrNum: number[] = [5, 4, 7, 2];
+const arrNum2: Array<number> = [5, 2];
+
+console.log(anyFloat)
