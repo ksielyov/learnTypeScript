b48d41e71df22db5a551207fe391b8920160ca49
ksielyov <mihailkiseliow@yandex.ru>
2020-12-06 22:15:26 +0500

    learned modals

--- interfaces.ts
+++ interfaces.ts
@@ -6,23 +6,42 @@ interface User {
         lat: number
         long: number
     }
+    userBackground?: string
 }
 
-const me: User = {
-    id: 1,
-    ava: '',
-    name: 'Mikhail',
+// const me: User = {
+//     id: 1,
+//     ava: '',
+//     name: 'Mikhail',
+//     position: {
+//         lat: 2.545465465465445,
+//         long: 54.654565454645
+//     }
+// }
+//
+// console.log(me)
+//
+// me.userBackground = null
+//
+// console.log(me)
+// let another = {} as User
+
+interface UserReducer extends User {
+    changeAva: () => string
+}
+
+const userModel: UserReducer = {
+    id: 2,
+    ava: null,
+    name: '',
     position: {
-        lat: 2.545465465465445,
-        long: 54.654565454645
+        lat: 55.4,
+        long: 2.2
+    },
+    changeAva: () : string => {
+        console.log(45645)
+        return '123'
     }
 }
 
-console.log(me)
-
-me.ava = null
-
-console.log(me)
-
-
-// let another = {} as User
+console.log(userModel)
