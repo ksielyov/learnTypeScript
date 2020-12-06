277511b1b780f7a11b5a9ca94381cde8d22f7f50
ksielyov <mihailkiseliow@yandex.ru>
2020-12-06 22:34:52 +0500

    Interfaces learnt

--- interfaces.ts
+++ interfaces.ts
@@ -1,63 +1,75 @@
-// interface User {
-//     readonly id: number
-//     ava: string | null
-//     name: string
-//     position: {
-//         lat: number
-//         long: number
-//     }
-//     userBackground?: string
-// }
-//
-// // const me: User = {
-// //     id: 1,
-// //     ava: '',
-// //     name: 'Mikhail',
+// // interface User {
+// //     readonly id: number
+// //     ava: string | null
+// //     name: string
 // //     position: {
-// //         lat: 2.545465465465445,
-// //         long: 54.654565454645
+// //         lat: number
+// //         long: number
+// //     }
+// //     userBackground?: string
+// // }
+// //
+// // // const me: User = {
+// // //     id: 1,
+// // //     ava: '',
+// // //     name: 'Mikhail',
+// // //     position: {
+// // //         lat: 2.545465465465445,
+// // //         long: 54.654565454645
+// // //     }
+// // // }
+// // //
+// // // console.log(me)
+// // //
+// // // me.userBackground = null
+// // //
+// // // console.log(me)
+// // // let another = {} as User
+// //
+// // interface UserReducer extends User {
+// //     changeAva: () => string
+// // }
+// //
+// // const userModel: UserReducer = {
+// //     id: 2,
+// //     ava: null,
+// //     name: '',
+// //     position: {
+// //         lat: 55.4,
+// //         long: 2.2
+// //     },
+// //     changeAva: () : string => {
+// //         console.log(45645)
+// //         return '123'
 // //     }
 // // }
 // //
-// // console.log(me)
-// //
-// // me.userBackground = null
-// //
-// // console.log(me)
-// // let another = {} as User
+// // console.log(userModel)
 //
-// interface UserReducer extends User {
-//     changeAva: () => string
+// interface UserClass {
+//     data: object
+//
+//     setData(data: {}): void
 // }
 //
-// const userModel: UserReducer = {
-//     id: 2,
-//     ava: null,
-//     name: '',
-//     position: {
-//         lat: 55.4,
-//         long: 2.2
-//     },
-//     changeAva: () : string => {
-//         console.log(45645)
-//         return '123'
+// class User implements UserClass {
+//     data: {}
+//
+//     setData(data: {}) {
+//         this.data = {ok: 4}
 //     }
 // }
 //
-// console.log(userModel)
+// console.log(User)
 
-interface UserClass {
-    data: object
+// type StyleSheetKeyType = string | number;
 
-    setData(data: {}): void
+interface styles {
+    [key: string] : string
 }
 
-class User implements UserClass {
-    data: {}
-
-    setData(data: {}) {
-        this.data = {ok: 4}
-    }
+const profileStyle: styles = {
+    borderWidth: '2px'
 }
 
-console.log(User)
+console.log(profileStyle)
