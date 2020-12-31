f690cd0f337de6203b987e0a809734e97a95253f
ksielyov <mihailkiseliow@yandex.ru>
2020-12-31 16:15:34 +0500

    Emums #2

--- enums.ts
+++ enums.ts
@@ -1,11 +1,19 @@
-enum userRole {
-    admin,
-    user,
-    moderator,
+// enum userRole {
+//     admin,
+//     user,
+//     moderator,
+// }
+//
+// let useRole: number = userRole.user;
+// let userStr: string = userRole[2];
+//
+// console.log(useRole)
+// console.log(userStr)
+
+enum languages {
+    js = 'JavaScript',
+    ts = 'TypeScript',
+    PHP = 'Govno'
 }
 
-let useRole: number = userRole.user;
-let userStr: string = userRole[2];
-
-console.log(useRole)
-console.log(userStr)
+console.log(languages.PHP)
