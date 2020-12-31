b56520b9afa1e3516c91ca7665451647ae5e8e35
ksielyov <mihailkiseliow@yandex.ru>
2020-12-31 16:27:04 +0500

    Functions

--- functions.ts
+++ functions.ts
@@ -1,37 +1,7 @@
-// // // const getUser = (user: number) : void => {
-// // //     console.log(user)
-// // // }
-// // //
-// // // getUser(2);
-// //
-// // let throwErr = (err: string): never => {
-// //     throw new Error(err)
-// // }
-// //
-// // // throwErr('Any error')
-// //
-// // type numArr = number[];
-// //
-// // let userIds: numArr = [2, 4, 5];
-// //
-// // console.log(userIds)
-//
-// type id = string | number;
-//
-// let userId: id = 2;
-//
-// console.log(userId)
-//
-// userId = 'test';
-//
-// console.log(userId)
+function getUserData(access: string): object {
+    return {
+        access: access
+    }
+}
 
-type types = null | undefined;
-
-let anyone: types = null
-
-console.log(anyone)
-
-anyone = undefined
-
-console.log(anyone)
+console.log(getUserData('assdasdasd6a54sd65a4s6d54a6s5d46a5s4d6a54sd'))
